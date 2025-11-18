ebf77af39c06c036b882825d0faf4d36dfc7431c
Aditya Lad <adityanlad@gmail.com>
2025-11-18 12:19:16 +0530

    js

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
+console.log('Lets write JS')
 
-const getSongs = async (params) => {
+
+
+const getSongs = async () => {
+   let a = await fetch('http://127.0.0.1:3000/songs/')
+    const response = await a.text(); 
+    // console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML=response
+    let as = div.getElementsByTagName("a")
+    let songs=[]
+    for (let i = 0; i < as.length; i++) {
+        const element = as[i];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href)
+        }
+        
+    }
+    return songs
     
-    let a = await fetch('http://127.0.0.1:3000/songs/')
-    const data = await a.text(); 
-    return data
+    
 }
 
 const main = async () => {
-    let songs = await getSongs()
-    console.log(songs)
-    
+    console.log(await getSongs())
 }
 main()
+
+
+
