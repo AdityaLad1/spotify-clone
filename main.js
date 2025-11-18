a696b5ca7846057032d2223d5163c5238f94ca05
Aditya Lad <adityanlad@gmail.com>
2025-11-18 12:01:02 +0530

    js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 
+const getSongs = async (params) => {
+    
+    let a = await fetch('http://127.0.0.1:3000/songs/')
+    const data = await a.text(); 
+    return data
+}
+
+const main = async () => {
+    let songs = await getSongs()
+    console.log(songs)
+    
+}
+main()
